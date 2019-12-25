88c3e1102f9d375eac12a5c4e1a94a52e04abb26
Arne Schroppe <arne.schroppe@gmx.de>
2019-12-25 18:37:32 +0100

    Added pattern matching

--- lib-src/Lib.hs
+++ lib-src/Lib.hs
@@ -55,7 +55,10 @@ denormalise (FExpr expr filters) = do
     findDecl ident ((i,e):rest) = if ident == i then Right e else findDecl ident rest
 
 filteredCombinations :: FilteredExpr -> [Value]
-filteredCombinations (FExpr expr filters) = combinations expr
+filteredCombinations (FExpr expr filters) =
+  let values = combinations expr in
+  let filteredValues = filterValues values filters in
+  filteredValues
 
 combinations :: Expr -> [Value]
 combinations expr = case expr of
@@ -71,6 +74,25 @@ listCombinations :: [[a]] -> [[a]]
 listCombinations (xs:[])  = map pure xs
 listCombinations (xs:xss) = [ a:b | a <- xs, b <- listCombinations xss ]
 listCombinations []       = []
+
+filterValues :: [Value] -> [ExprFilter] -> [Value]
+filterValues values filters = filter (not . (flip matchesFilters) filters) values
+
+matchesFilters :: Value -> [ExprFilter] -> Bool
+matchesFilters val filters = any (matchesFilter val) filters
+
+matchesFilter :: Value -> ExprFilter -> Bool
+matchesFilter val (EFilter _ pat) = matchesPattern val pat
+
+matchesPattern :: Value -> Pattern -> Bool
+matchesPattern (VTuple vals) (PTuple pats) = (length vals == length pats) && and (zipWith matchesPattern vals pats)
+matchesPattern (VVariant i Nothing) (PVariant IPWildcard Nothing)         = True
+matchesPattern (VVariant i (Just val)) (PVariant IPWildcard (Just pat))   = matchesPattern val pat
+matchesPattern (VVariant i Nothing) (PVariant (IPIdent pi) Nothing)       = (i == pi)
+matchesPattern (VVariant i (Just val)) (PVariant (IPIdent pi) (Just pat)) = (i == pi) && matchesPattern val pat
+matchesPattern (VTuple _) (PVariant IPWildcard Nothing)                   = True
+matchesPattern (VVariant i (Just _)) (PVariant IPWildcard Nothing)        = True
+matchesPattern _ _                                                        = False
 
 prettyPrint :: Value -> String
 prettyPrint val = case val of
